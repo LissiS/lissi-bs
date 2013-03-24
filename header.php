52cda0bae6cf598075d04c660ff9d0ea3a4fbad2
Lissi <lissi31@gmail.com>
2013-03-24 20:28:16 +0000

    Removed login and pulled nav right

--- header.php
+++ header.php
@@ -36,15 +36,10 @@
             <span class="icon-bar"></span>
           </a>
           <a class="brand" href="<?php echo site_url(); ?>"><?php bloginfo('name'); ?></a>
-          <div class="nav-collapse collapse">
+          <div class="nav-collapse collapse pull-right">
             <ul class="nav">
               <?php wp_list_pages(array('title_li' => '', 'exclude' => 4)); ?>
             </ul>
-            <form class="navbar-form pull-right">
-              <input class="span2" type="text" placeholder="Email">
-              <input class="span2" type="password" placeholder="Password">
-              <button type="submit" class="btn">Sign in</button>
-            </form>
           </div><!--/.nav-collapse -->
         </div>
       </div>
