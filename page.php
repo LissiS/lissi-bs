fac48e58a0cc0e888f697aad27beeb8f81f53535
Lissi <lissi31@gmail.com>
2013-03-25 20:12:19 +0000

    Sidebar and layout

--- page.php
+++ page.php
@@ -1,12 +1,24 @@
 <?php get_header(); ?>
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<div class="row">
 
-  <h1><?php the_title(); ?></h1>  
-  <?php the_content(); ?>
+  <span class="span8">
 
-<?php endwhile; else: ?>
-  <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-<?php endif; ?>
+  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+      <h1><?php the_title(); ?></h1>  
+      <?php the_content(); ?>
+
+    <?php endwhile; else: ?>
+      <p><?php _e('Sorry, this page does not exist.'); ?></p>
+    <?php endif; ?>
+
+  </span>
+
+  <span class="8">
+      <?php get_sidebar(); ?> 
+  </span>
+
+</div>
 
 <?php get_footer(); ?>
